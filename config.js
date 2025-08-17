bf2cd373d13aad7aed70b9e97a76674e6e605328
boahstrike25 <brucece12008@outlook.com>
2025-08-17 13:41:18 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // Set an external JSON URL here (or use ?data= URL param)
-window.DATA_URL = "https://raw.githubusercontent.com/boahstrike25/yopac-grants-data/main/grants.json";
+window.DATA_URL = "https://raw.githubusercontent.com/boahstrike25/yopac-grants-data/refs/heads/main/grants.json";
+
